7667154ac930e0d2e962c6255317f2bae6a14e67
slumpy <96844872+Matt-Wiszniewski@users.noreply.github.com>
2024-02-17 14:43:17 -0500

    Create crypto_parameter_test.c
    testing the parameters

---
+++ component/inc/crypto_parameter_test.c
@@ -0,0 +1,18 @@
+//testing the parameters
+#include "crypto_parameters.h"
+bool testParams(){
+  CryptoParameters cryptoConfig;
+  //any test conditions
+  return true;
+}
+
+// int main(){
+//   if(testParams()){
+//     printf("works");
+//   }else{
+//     printf("Fail")
+//   }
+//   return 0;
+// }
+
+  
