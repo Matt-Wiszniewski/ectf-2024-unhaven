593309050b1bab3c57c626885a3ee8572153c2ae
slumpy <96844872+Matt-Wiszniewski@users.noreply.github.com>
2024-02-17 14:28:12 -0500

    Create crypto_parameters.h
    Crypto parameters file

---
+++ component/inc/crypto_parameters.h
@@ -0,0 +1,18 @@
+//crypto_parameters.h
+
+#ifndef CRYPTO_PARAMETERS_H
+#define CRYPTO_PARAMETERS_H
+
+#include <stdint.h>
+
+//crptographic cryptographic
+typdef struct { 
+  unit8_t key[16];
+  //cryptographic algorithms 
+  //modes of operation
+  //authentication parameters
+  //etc
+} CryptoParameters;
+
+
+#endif
