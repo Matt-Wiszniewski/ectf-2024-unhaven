0ed607ec134f0039cb730d49a85ece2774eae584
slumpy <96844872+Matt-Wiszniewski@users.noreply.github.com>
2024-02-17 14:30:41 -0500

    Update board_link.h
    update

--- component/inc/board_link.h
+++ component/inc/board_link.h
@@ -15,6 +15,7 @@
 #define __BOARD_LINK__
 
 #include "simple_i2c_peripheral.h"
+#include "crypto_parameters.h"
 
 /******************************** MACRO DEFINITIONS ********************************/
 // Last byte of the component ID is the I2C address
